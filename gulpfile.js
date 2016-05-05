449bc3e90058b371bbb1b15a56db0d8ef0606d5f
lorak <k.zysk@zoho.com>
2016-05-05 17:37:03 +0200

    chore: add task css

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,21 @@ var autoprefixer = require('gulp-autoprefixer');
 var minifyCSS = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-var concat = require('gulp-concat');
+var concatify = require('gulp-concat');
+
+var paths = {
+	stylesheets: ['src/css/*.css'],
+};
+
+
+gulp.task('css', function() {
+    gulp.src(paths.stylesheets)
+        .pipe(stylus({compress: false, paths: ['src/css']}))
+        .pipe(autoprefixer())
+        .pipe(minifyCSS())
+        .pipe(concatify('styles.min.css'))
+        .pipe(gulp.dest('dist/css'))
+});
 
 gulp.task('default', function() {
     console.log("Yeah, I'm gulpin!");
