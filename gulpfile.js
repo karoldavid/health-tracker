04250d4d123424de352abab60d953193f9fbb546
lorak <k.zysk@zoho.com>
2016-05-05 15:07:06 +0200

    chore: require plugins gulp-stylus, gulp-autoprefixer, gulp-minify-css, gulp-uglify, gulp-rename, gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,10 @@
 var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
 
 gulp.task('default', function() {
     console.log("Yeah, I'm gulpin!");
